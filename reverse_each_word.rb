cfe6a81423ffc15cdd8da5e6d3481d1cd4e5d805
John Karrell Speck <speckworks@gmail.com>
2019-08-15 15:21:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,7 +6,6 @@
 def reverse_each_word(sentence1)
    sentence1.split.each {|word| word.reverse}.join
  end
-sentence1 = "Hello, how are you?"
  
 #   a = [ "a", "b", "c", "d" ]
 # a.collect {|x| x + "!" }   #=> ["a!", "b!", "c!", "d!"]
