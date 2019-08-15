35ffcc115ac4f54a8d70aaab2fd66e3f71322ac7
John Karrell Speck <speckworks@gmail.com>
2019-08-15 14:26:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,9 +11,9 @@ def reverse_each_word(sentence1)
   end
 end
   
-  a = [ "a", "b", "c", "d" ]
-a.collect {|x| x + "!" }   #=> ["a!", "b!", "c!", "d!"]
-a  
-#   oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", "Doc", "Lemonjello", "Sleepy" ]
+#   a = [ "a", "b", "c", "d" ]
+# a.collect {|x| x + "!" }   #=> ["a!", "b!", "c!", "d!"]
+# a  
+# #   oppressed_workers = ["Dopey", "Sneezy", "Happy", "Angry", "Doc", "Lemonjello", "Sleepy" ]
 # oppressed_workers.each do |oppressed_worker|
 #   puts "#{oppressed_worker.capitalize} wants to start a union!"
