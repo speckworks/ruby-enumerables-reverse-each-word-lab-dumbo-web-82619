003a7d923967d578a4f8fecfa18286968211792c
John Karrell Speck <speckworks@gmail.com>
2019-08-15 14:16:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ reverse_each_word(sentence1)
   sentence1 = "Hello there, and how are you?"
   sentence1.split("")
   sentence1.each do |sentence1|
-    puts #{sentence1}.reverse
+    puts #{sentence1.reverse}
   end
 end
   
