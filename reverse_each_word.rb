7029c9ba2c244628f09510d8d200fd3d6ba2780a
John Karrell Speck <speckworks@gmail.com>
2019-08-15 15:28:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@
 #     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
 
 def reverse_each_word(sentence1)
-   sentence1.split.each {|word| word.reverse}.join(" ")
+   sentence1.split.collect {|word| word.reverse}.join(" ")
  end
  
  
